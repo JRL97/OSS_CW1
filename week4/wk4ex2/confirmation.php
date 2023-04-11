ea514a6f4e69bc262faa4a349557464ccc86724d
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-11 02:14:10 +0100

    missing colon?

--- week4/wk4ex2/confirmation.php
+++ week4/wk4ex2/confirmation.php
@@ -4,6 +4,6 @@ $price = 15.75;
 $total = $price * $_SESSION["qty"];
    echo "<h2> Your order qty is $_SESSION[qty] </h2></br>";
    echo "<p> $_SESSION[qty] x £$price = £$total </p>";
-   echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+   echo "<h2> and the selected colour is $_POST[selcolour].</h2>";
    echo "<h2> Price to Pay is £$total</h2>";
 ?>
