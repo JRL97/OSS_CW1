0d90ade8267abc0360efdf0c68d3e3413a110002
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-11 02:09:21 +0100

    Broken

--- week4/wk4ex2/confirmation.php
+++ week4/wk4ex2/confirmation.php
@@ -3,7 +3,7 @@ session_start();
 $price = 15.75;
 $total = $price * $_SESSION["qty"];
    echo "<h2> Your order qty is $_SESSION[qty] </h2></br>";
-   echo "<p> $_SESSION[qty] * £$price = £$total </p>";
+   echo "<p> $_SESSION[qty] x £$price = £$total </p>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
    echo "<h2> Price to Pay is £$total</h2>";
 ?>
