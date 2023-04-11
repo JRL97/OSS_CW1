37bf9284a01477f02ec99c31fcbf9c2b9c276ac1
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-11 01:41:55 +0100

    missing quotes

--- week4/wk4ex1.2/selectcolour.php
+++ week4/wk4ex1.2/selectcolour.php
@@ -7,7 +7,7 @@
       ?>
 
       <form action="confirmation.php"  method="post">
-	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
+	Select the colour for the <?php echo $_POST['selqty'] ?> widgets you are ordering
    	<select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>
