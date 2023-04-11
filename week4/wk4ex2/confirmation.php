64e5664ca739773a4d911be99d8d084fa5adc851
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-11 02:04:19 +0100

    error in what folders corrected

--- week4/wk4ex2/confirmation.php
+++ week4/wk4ex2/confirmation.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+$price = 15.75;
    echo "<h2> Your order qty is $_SESSION[qty] </h2></br>";
+   echo "<p> $_SESSION[qty] * £$price = £$total </p>";
    echo "<h2> and the selected colour is $_POST[selcolour].</h2>"
+   echo "<h2> Price to Pay is £$total</h2>";
 ?>
