fab94faf1255f6033be5162e94556d9825dab339
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-11 01:46:15 +0100

    wk4ex1.3

--- week4/wk4ex1.3/selectcolour.php
+++ week4/wk4ex1.3/selectcolour.php
@@ -1,8 +1,12 @@
 <html>
   <head><title>Select colour page</title></head>
     <body>
+      <?php
+      session_start();
+      $_SESSION['qty'] = $_POST['selqty'];
+      ?>
       <form action="confirmation.php"  method="post">
-	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
+	Select the colour for the <?php echo $_POST['selqty'] ?> widgets you are ordering
    	<select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>
