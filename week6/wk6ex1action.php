f56e3fbdb96be8127010707d23ed28bb28d67e83
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-12 20:24:35 +0100

    wk6ex1 and 2 working

--- week6/wk6ex1action.php
+++ week6/wk6ex1action.php
@@ -1,18 +1,19 @@
 <?php
 	$sql = "INSERT INTO test (name,email,phone_number) ";
 	$sql = $sql . " values ('$_POST[txtName]','$_POST[txtEmail]','$_POST[txtPhoneNumber]')";
+	echo $sql;
 
 	// Connect to server and select database
 	
     $mysqli = new mysqli('localhost','root','','db1_jess');
    
 	// Execute sql statement		
-
+    $result = $mysqli -> query($sql);
 	$sql = "SELECT * from test";
 	
 	// Execute sql statement
 	
-    $result = $mysqli->query($sql);
+    $result = $mysqli -> query($sql);
 
 	while ($row = mysqli_fetch_assoc($result))
 	{
