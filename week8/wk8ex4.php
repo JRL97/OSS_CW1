b113bb2b165c657d3e2b76aa521a826569105c14
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-13 23:55:16 +0100

    wk8 ex4 more

--- week8/wk8ex4.php
+++ week8/wk8ex4.php
@@ -1,5 +1,19 @@
-<?php     
+<?php
      $conn = mysqli_connect("localhost", "22133230", "mysqluser", "db3_22133230");
+
+     if ($_POST[selweek]){
+       $sql = "select * from lotto where wk=$_POST[selweek];";
+       $result = mysqli_query($conn, $sql);
+       $row = mysqli_fetch_array($result);
+
+       echo "Number 1 is  $row[number1]<br/>"; 
+       echo "Number 2 is  $row[number2]<br/>"; 
+       echo "Number 3 is  $row[number3]<br/>"; 
+       echo "Number 4 is  $row[number4]<br/>"; 
+       echo "Number 5 is  $row[number5]<br/>"; 
+       echo "Number 6 is  $row[number6]<br/>"; 
+     }
+     else {
        $sql = "select * from lotto;";
        $result = mysqli_query($conn, $sql);
 
@@ -12,4 +26,5 @@
        echo "</select><br/>";
        echo "<input type='submit' value='Select' />";
        echo "</form>";
+     }
 ?>
