23889ca2bfb3e21bd9ebafa0bb4546e6bbaca2d1
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-13 21:49:09 +0100

    query

--- week7/getjpg.php
+++ week7/getjpg.php
@@ -4,7 +4,8 @@
 
   $conn = mysqli_connect("localhost", "22133230","mysqluser", "db3_22133230 ");
 
-  $sql = "SELECT image FROM monster WHERE id='" . $_GET[id] ."';";
+  //$sql = "SELECT image FROM monster WHERE id='" . $_GET[id] ."';";
+  $sql = "SELECT image FROM monster WHERE id=1";
 
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result);
