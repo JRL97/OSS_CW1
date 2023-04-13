dfbeab61f8c92bff9d7073916b8cc1c93189c5f9
JRL97 <91621546+JRL97@users.noreply.github.com>
2023-04-13 19:21:08 +0100

    dchduhcodicfh

--- week6/wk6ex6.php
+++ week6/wk6ex6.php
@@ -1,7 +1,7 @@
 <?php
 	include("myfunctions.inc");
 	html_header("My second function demo");
-	echo "I pay £ " . calculatetax(13450) . " tax";
+	echo "I pay £ " . calculatetax(13450,12,1500) . " tax";
 	html_footer();
 ?>
 
